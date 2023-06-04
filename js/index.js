4929a0f1136a568b9cb7b270c219a857b04a8773
Eder Natan <edernatan124@gmail.com>
2023-06-04 01:33:44 -0300

    nav bar ok !!

---
+++ js/index.js
@@ -0,0 +1,24 @@
+/*------------ SHOW MENU-------------*/
+const showMneu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+    /*valida se se existe as variaveis*/
+    if (toggle && nav) {
+        /*aqui adiciono a class show-menu na div com a class nav__menu*/
+        toggle.addEventListener('click', () => {
+            /*adicina a class show-menu na div que tem o id nav-menu */
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMneu('nav-toggle', 'nav')
+
+
+/*------------ REMOVER MENU MOBILE -------------*/
+const navLink = document.querySelectorAll('.nav__link')
+function linkAction() {
+    const navMenu = document.getElementById('nav')
+    /*Quando um link ou o botão close for clicado a class show-menu será removido*/
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
